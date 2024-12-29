75438bb7d15507c671514b17c44522d326f1e10c
Mikelmv18 <mvangjeli22>
2024-12-29 16:43:12 +0100

    String format of book object formatted, some comments fixed in dailystatistics testing class added and new testing class added

--- src/book/Book.java
+++ src/book/Book.java
@@ -5,6 +5,7 @@ package book;//
 
 import exceptions.ISBNnotValidException;
 
+import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
 
@@ -22,8 +23,12 @@ public class Book implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    public Book(String ISBN, String author, String title, String book_category,
-               String purchased_date, double purchased_price, double selling_price, int stock) throws ISBNnotValidException {
+    public Book(String ISBN, String author, String title,
+                String book_category,
+               String purchased_date, double purchased_price,
+                double selling_price, int stock) throws ISBNnotValidException,
+            IOException {
+
         this.ISBN = ISBN;
         this.author = author;
         this.title = title;
@@ -109,7 +114,10 @@ public class Book implements Serializable {
         this.stock = stock;
     }
 
+    @Override
     public String toString() {
-        return "ISBN=" + this.ISBN + "   author=" + this.author + "   title=" + this.title + "   book_category=" + this.book_category + "   purchased_date=" + this.purchased_date + "   purchased_price=" + this.purchased_price + "   stock=" + this.stock + "\n";
+        return String.format("ISBN=%s author=%s title=%s book_category=%s purchased_date=%s purchased_price=%.1f stock=%d",
+                this.ISBN, this.author, this.title, this.book_category,
+                this.purchased_date, this.purchased_price, this.stock);
     }
 }
